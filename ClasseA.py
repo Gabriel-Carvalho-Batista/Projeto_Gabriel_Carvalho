ae8074b61cc470ec3ff7aafd5868ec83e6b2d48d
Gabriel Carvalho Batista <gatraxcb@gmail.com>
2026-09-22 09:05:07 -0300

    Criação das classes A e B

---
+++ ClasseA.py
@@ -0,0 +1,31 @@
+class ClasseA:
+    def __init__(self, a1: int = 0, a2: float = 0.0):
+        self.__A1 = a1
+        self.__A2 = a2
+
+    # Getters e Setters
+    def getA1(self) -> int:
+        return self.__A1
+
+    def setA1(self, a1: int):
+        self.__A1 = a1
+
+    def getA2(self) -> float:
+        return self.__A2
+
+    def setA2(self, a2: float):
+        self.__A2 = a2
+
+    # Métodos da classe
+    def MA1(self):
+        print("MA1")
+
+    def MA2(self):
+        print("MA2")
+
+
+if __name__ == "__main__":
+    obj = ClasseA(10, 3.14)
+    obj.MA1()
+    obj.MA2()
+    print(f"A1 = {obj.getA1()}, A2 = {obj.getA2()}")
---
+++ ClasseB.py
@@ -0,0 +1,31 @@
+class ClasseB:
+    def __init__(self, b1: int = 0, b2: float = 0.0):
+        self.__B1 = b1
+        self.__B2 = b2
+
+    # Getters e Setters
+    def getB1(self) -> int:
+        return self.__B1
+
+    def setB1(self, b1: int):
+        self.__B1 = b1
+
+    def getB2(self) -> float:
+        return self.__B2
+
+    def setB2(self, b2: float):
+        self.__B2 = b2
+
+    # Métodos da classe
+    def MB1(self):
+        print("MB1")
+
+    def MB2(self):
+        print("MB2")
+
+
+if __name__ == "__main__":
+    obj = ClasseB(20, 2.71)
+    obj.MB1()
+    obj.MB2()
+    print(f"B1 = {obj.getB1()}, B2 = {obj.getB2()}")
